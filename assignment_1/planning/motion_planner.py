05367655f91f7b14d3dfaf57fe5424d68f68959e
Pochomo <xorkd119@gmail.com>
2025-05-23 22:51:23 +0900

    motion_planner 주석추가

--- assignment_1/planning/motion_planner.py
+++ assignment_1/planning/motion_planner.py
@@ -41,28 +41,36 @@ class MotionPlanner(threading.Thread):
                 print("++++++++motion_planner+++++++++")
 
             sleep(self.period)
-
+    
     def publish_frenet_path(self, frenet_path):
+        '''
+        Frenet 경로를 ROS 메시지로 퍼블리쉬
+        '''
         path_msg = Path()
         path_msg.header.stamp = rospy.Time.now()
         path_msg.header.frame_id = "map"
 
+        # (x, y, yaw) 좌표계를 PoseStamped 메시지로 변환
         for x, y, yaw in zip(frenet_path.x, frenet_path.y, frenet_path.yaw):
             pose = PoseStamped()
+            # 헤더를 현재시간, "map"으로 설정
             pose.header.stamp = rospy.Time.now()
             pose.header.frame_id = "map"
             pose.pose.position.x = x
             pose.pose.position.y = y
             pose.pose.position.z = 0.0
 
+            # yaw를 Quaternion으로 변환 tf를 사용한다. 
             quaternion = tf.transformations.quaternion_from_euler(0, 0, yaw)
             pose.pose.orientation.x = quaternion[0]
             pose.pose.orientation.y = quaternion[1]
             pose.pose.orientation.z = quaternion[2]
             pose.pose.orientation.w = quaternion[3]
 
+            # PoseStamped 메시지를 path_msg의 poses에 추가
             path_msg.poses.append(pose)
 
+        # ROS 메시지를 퍼블리쉬
         self.path_pub.publish(path_msg)
 
     def generate_frenet_path(self):
